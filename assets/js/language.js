0c2b86b926c9e88a72076eb1be8e54a9bd97cbd9
Sebastián Ibagué Castro <65819119+BlueFireA25@users.noreply.github.com>
2022-01-09 11:40:47 -0500

    Add files via upload

---
+++ assets/js/language.js
@@ -0,0 +1,218 @@
+var dictionary = {
+    'Home':{
+        'en': 'Home',
+        'es': 'Inicio'
+    },
+    'About':{
+        'en': 'About',
+        'es': 'Acerca de'
+    },
+    'Products':{
+        'en': 'Products',
+        'es': 'Productos'
+    },
+    'FAQs':{
+        'en': 'FAQs',
+        'es': 'FAQs'
+    },
+    'Contact Us':{
+        'en': 'Contact Us',
+        'es': 'Contáctenos'
+    },
+    'Send A Gift':{
+        'en': 'Send A Gift',
+        'es': 'Enviar un regalo'
+    },
+    'Christmas':{
+        'en': 'Christmas',
+        'es': 'de Navidad'
+    },
+    'Home Description':{
+        'en': 'In these end of the year holidays send a gift to your loved one and share the happiness at Christmas.',
+        'es': 'En estas fiestas de fin de año envía un regalo a tu ser querido y comparte la alegría en Navidad.'
+    },
+    'Explore':{
+        'en': 'Explore',
+        'es': 'Explorar'
+    },
+    'About Title':{
+        'en': 'Sharing is the best gift you can give',
+        'es': 'Compartir es el mejor regalo que puedes dar'
+    },
+    'Follow Us':{
+        'en': 'Follow Us',
+        'es': 'Síganos'
+    },
+    'About Description':{
+        'en': 'Sharing these holidays is the best gift you can give, give a present or share your love with the people you love the most and celebrate with great happiness.',
+        'es': 'Compartir estas fiestas es el mejor regalo que puedes dar, obsequia o comparte tu amor con las personas que más quieres y celebra con mucha felicidad.'
+    },
+    'About Details 1':{
+        'en': 'We always deliver on time.',
+        'es': 'Siempre entregamos a tiempo.'
+    },
+    'About Details 2':{
+        'en': 'We give you guides to protect and care for your gifts.',
+        'es': 'Te damos guías para proteger y cuidar tus regalos.'
+    },
+    'About Details 3':{
+        'en': 'We always come over for a check-up after sale.',
+        'es': 'Siempre venimos para un chequeo después de la venta. '
+    },
+    'About Details 4':{
+        'en': '100% money back guaranteed.',
+        'es': '100% devolución de dinero garantizada.'
+    },
+    'Shop Now':{
+        'en': 'Shop Now',
+        'es': 'Compra ahora'
+    },
+    'Steps Title':{
+        'en': 'Steps to start your gifts off right',
+        'es': 'Pasos para empezar bien tus regalos'
+    },
+    'Steps Card Title 1':{
+        'en': 'Choose Gift',
+        'es': 'Elige regalo'
+    },
+    'Steps Card Title 2':{
+        'en': 'Place an order',
+        'es': 'Haz un pedido'
+    },
+    'Steps Card Title 3':{
+        'en': 'Get gifts delivered',
+        'es': 'Recibir regalos entregados'
+    },
+    'Steps Card Description 1':{
+        'en': 'We have several varieties gifts you can choose from.',
+        'es': 'Tenemos varias variedades de regalos entre los que puedes elegir.'
+    },
+    'Steps Card Description 2':{
+        'en': 'Once your order is set, we move to the next step which is the shipping.',
+        'es': 'Una vez que se establece su pedido, pasamos al siguiente paso, que es el envío.'
+    },
+    'Steps Card Description 3':{
+        'en': 'Our delivery process is easy, you receive the gifts direct to your door.',
+        'es': 'Nuestro proceso de entrega es fácil, recibes los regalos directo a tu puerta.'
+    },
+    'Products Title':{
+        'en': 'Check out our products',
+        'es': 'Echa un vistazo a nuestros productos'
+    },
+    'Products Description':{
+        'en': 'Here are some selected gifts from our showroom, all are in excellent condition. Buy and enjoy best quality.',
+        'es': 'Aquí hay algunos regalos seleccionados de nuestra sala de exposición, todos están en excelentes condiciones. Compra y disfruta de la mejor calidad.'
+    },
+    'Product 1':{
+        'en': 'Snow globe',
+        'es': 'Globo de nieve'
+    },
+    'Product 2':{
+        'en': 'Candy',
+        'es': 'Caramelo'
+    },
+    'Product 3':{
+        'en': 'Angel mask',
+        'es': 'Máscara de ángel '
+    },
+    'Product 4':{
+        'en': 'Sphere',
+        'es': 'Esfera'
+    },
+    'Product 5':{
+        'en': 'Surprise gift',
+        'es': 'Regalo sorpresa'
+    },
+    'Questions Title':{
+        'en': 'Some common questions were often asked',
+        'es': 'A menudo se hacen algunas preguntas comunes'
+    },
+    'Question 1':{
+        'en': 'How can I track my shipment?',
+        'es': 'Cómo puedo rastrear mi envío?'
+    },
+    'Question 2':{
+        'en': 'What do I do if the shipment does not arrive?',
+        'es': 'Qué hago si el envío no llega?'
+    },
+    'Question 3':{
+        'en': 'How is the gift payment process?',
+        'es': 'Cómo es el proceso de pago de regalos?'
+    },
+    'Question 4':{
+        'en': 'How do i choose a Gift?',
+        'es': 'Cómo elijo un regalo? '
+    },
+    'Question 5':{
+        'en': 'How do I change the Gifts?',
+        'es': 'Cómo cambio los regalos?'
+    },
+    'Question 6':{
+        'en': 'What do I do if a gift arrived broken or defective?',
+        'es': 'Qué hago si un regalo llegó roto o defectuoso?'
+    },
+    'Contact Title':{
+        'en': 'Contact us',
+        'es': 'Contacta con nosotros'
+    },
+    'Contact Subtitle 1':{
+        'en': 'Call us for instant support',
+        'es': 'Llámenos para soporte instantáneo'
+    },
+    'Contact Subtitle 2':{
+        'en': 'Write us by mail',
+        'es': 'Escríbenos por correo'
+    },
+    'Subject':{
+        'en': 'Subject',
+        'es': 'Asunto'
+    },
+    'Message':{
+        'en': 'Message',
+        'es': 'Mensaje'
+    },
+    'Send Message':{
+        'en': 'Send Message',
+        'es': 'Enviar Mensaje'
+    },
+    'Footer Title':{
+        'en': 'Subscribe to our newsletter to stay update',
+        'es': 'Suscríbete a nuestro boletín para mantenerte actualizado'
+    },
+    'Our Address':{
+        'en': 'Our Address',
+        'es': 'Nuestra dirección'
+    },
+    'We accept all credit cards':{
+        'en': 'We accept all credit cards',
+        'es': 'Aceptamos todas las tarjetas de crédito'
+    },
+    'All rights reserved':{
+        'en': 'All rights reserved.',
+        'es': 'Todos los derechos reservados.'
+    }
+};
+
+var langs = ['en', 'es'];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+var varTranslator = true;
+
+window.change_lang = function() {
+    current_lang_index = ++current_lang_index % 2;
+    current_lang = langs[current_lang_index];
+    translate();
+}
+
+function translate() {
+        //var newsletterInput = document.getElementById('newsletter__input');
+        // varTranslator?newsletterInput.placeholder = "Enter your email":newsletterInput.placeholder = "Introduce tu correo electrónico";
+        //varTranslator=!varTranslator;
+
+        $("[data-translate]").each(function(){
+        var key = $(this).data('translate');
+        $(this).html(dictionary[key][current_lang] || "N/A");
+    });
+}
+
+translate();
